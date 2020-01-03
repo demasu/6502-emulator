403cca426d9692b8f44a6211630272ab0b36b13d
Demasu <kc9guu@gmail.com>
2020-01-03 17:02:39 -0600

    Updated bus class

--- Bus.cpp
+++ Bus.cpp
@@ -3,25 +3,57 @@
 Bus::Bus() {
     // Connect CPU to the communication bus
     cpu.ConnectBus(this);
-
-    // Clear RAM contents just in case
-    for ( auto &i : ram ) {
-        i = 0x00;
-    }
 }
 
 Bus::~Bus() {}
 
-void Bus::write(uint16_t addr, uint8_t data) {
-    if ( addr >= 0x0000 && addr <= 0xFFFF ) {
-        ram[addr] = data;
+void Bus::cpuWrite(uint16_t addr, uint8_t data) {
+    if (cart->cpuWrite(addr, data)) {
+    }
+    else if (addr >= 0x0000 && addr <= 0x1FFF) {
+        // System RAM
+        cpuRam[addr & 0x07FF] = data;
+    }
+    else if (addr >= 0x2000 && addr <= 0x3FFF) {
+        // PPU Address range
+        ppu.cpuWrite(addr & 0x0007, data);
     }
 }
 
-uint8_t Bus::read(uint16_t addr, bool bReadOnly) {
-    if ( addr >= 0x0000 && addr <= 0xFFFF ) {
-        return ram[addr];
+uint8_t Bus::cpuRead(uint16_t addr, bool bReadOnly) {
+    uint8_t data = 0x00;
+    if (cart->cpuRead(addr, data)) {
+    }
+    else if (addr >= 0x0000 && addr <= 0x1FFF) {
+        // System RAM
+        data = cpuRam[addr & 0x07FF];
+    }
+    else if (addr >= 0x2000 && addr <= 0x3FFF) {
+        data = ppu.cpuRead(addr & 0x0007, bReadOnly);
     }
 
-    return 0x00;
+    return data;
+}
+
+void Bus::insertCartridge(const std::shared_ptr<Cartridge>& cartridge) {
+    this->cart = cartridge;
+    ppu.ConnectCartridge(cartridge);
+}
+
+void Bus::reset() {
+    cpu.reset();
+    nSystemClockCounter = 0;
+}
+
+void Bus::clock() {
+    // Whatever calls this is responsible for the overall clocking frequency.
+    // The PPU runs 3 times faster than the CPU clock, so we want to clock it every time
+    ppu.clock();
+
+    // Since the CPU is slower, call it every third time
+    if (nSystemClockCounter % 3 == 0) {
+        cpu.clock();
+    }
+
+    nSystemClockCounter++;
 }
