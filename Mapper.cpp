51670f5539278ce7c0ebc0351a636cbd393314b6
Demasu <kc9guu@gmail.com>
2020-01-03 19:07:42 -0600

    Update mapper classes

--- Mapper.cpp
+++ Mapper.cpp
@@ -3,6 +3,10 @@
 Mapper::Mapper(uint8_t prgBanks, uint8_t chrBanks) {
     nPRGBanks = prgBanks;
     nCHRBanks = chrBanks;
+
+    //reset(); // Causes an error of [Pure virtual function called!]
 }
 
 Mapper::~Mapper() {}
+
+void Mapper::reset() {}
